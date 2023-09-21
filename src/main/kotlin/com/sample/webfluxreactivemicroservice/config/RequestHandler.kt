8a08855dba33c08d93a6ec415a5af55472275595
jeanfernandesComp1997 <jeanfernandes10@hotmail.com>
2023-09-20 22:08:27 -0300

    feat: add spring webflux router functions example

---
+++ src/main/kotlin/com/sample/webfluxreactivemicroservice/config/RequestHandler.kt
@@ -0,0 +1,57 @@
+package com.sample.webfluxreactivemicroservice.config
+
+import com.sample.webfluxreactivemicroservice.dto.InputFailedValidationResponse
+import com.sample.webfluxreactivemicroservice.dto.MathSquareResponse
+import com.sample.webfluxreactivemicroservice.dto.MultiplyRequest
+import com.sample.webfluxreactivemicroservice.exception.InputValidationException
+import com.sample.webfluxreactivemicroservice.service.MathService
+import com.sample.webfluxreactivemicroservice.service.ReactiveMathService
+import org.springframework.http.MediaType
+import org.springframework.stereotype.Service
+import org.springframework.web.reactive.function.server.ServerRequest
+import org.springframework.web.reactive.function.server.ServerResponse
+import reactor.core.publisher.Mono
+
+@Service
+class RequestHandler(
+    private val mathService: ReactiveMathService
+) {
+
+    fun squareHandler(serverRequest: ServerRequest): Mono<ServerResponse> {
+        val input = serverRequest.pathVariable("input").toInt()
+        val response = this.mathService.findSquare(input)
+        return ServerResponse.ok().body(response, MathSquareResponse::class.java)
+    }
+
+    fun tableHandler(serverRequest: ServerRequest): Mono<ServerResponse> {
+        val input = serverRequest.pathVariable("input").toInt()
+        val response = this.mathService.multiplicationTable(input)
+        return ServerResponse.ok().body(response, MathSquareResponse::class.java)
+    }
+
+    fun tableStreamHandler(serverRequest: ServerRequest): Mono<ServerResponse> {
+        val input = serverRequest.pathVariable("input").toInt()
+        val response = this.mathService.multiplicationTable(input)
+        return ServerResponse
+            .ok()
+            .contentType(MediaType.TEXT_EVENT_STREAM)
+            .body(response, MathSquareResponse::class.java)
+    }
+
+    fun multiplyHandler(serverRequest: ServerRequest): Mono<ServerResponse> {
+        val request = serverRequest.bodyToMono(MultiplyRequest::class.java)
+        val response = mathService.multiply(request)
+        return ServerResponse
+            .ok()
+            .body(response, MathSquareResponse::class.java)
+    }
+
+    fun squareHandlerWithValidation(serverRequest: ServerRequest): Mono<ServerResponse> {
+        val input = serverRequest.pathVariable("input").toInt()
+        if (input < 10 || input > 20) {
+            return Mono.error(InputValidationException(input))
+        }
+        val response = this.mathService.findSquare(input)
+        return ServerResponse.ok().body(response, MathSquareResponse::class.java)
+    }
+}
---
+++ src/main/kotlin/com/sample/webfluxreactivemicroservice/config/RouterConfig.kt
@@ -0,0 +1,50 @@
+package com.sample.webfluxreactivemicroservice.config
+
+import com.sample.webfluxreactivemicroservice.dto.InputFailedValidationResponse
+import com.sample.webfluxreactivemicroservice.exception.InputValidationException
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.reactive.function.server.RouterFunction
+import org.springframework.web.reactive.function.server.RouterFunctions
+import org.springframework.web.reactive.function.server.ServerRequest
+import org.springframework.web.reactive.function.server.ServerResponse
+import reactor.core.publisher.Mono
+import java.util.function.BiFunction
+
+@Configuration
+class RouterConfig(
+    private val requestHandler: RequestHandler
+) {
+
+    @Bean
+    fun highLevelRouter(): RouterFunction<ServerResponse> {
+        return RouterFunctions
+            .route()
+            .path("router", this::serverResponseRouterFunction)
+            .build()
+    }
+
+    private fun serverResponseRouterFunction(): RouterFunction<ServerResponse> {
+        return RouterFunctions
+            .route()
+            .GET("square/{input}", requestHandler::squareHandler)
+            .GET("table/{input}", requestHandler::tableHandler)
+            .GET("table/{input}/stream", requestHandler::tableStreamHandler)
+            .POST("multiply", requestHandler::multiplyHandler)
+            .GET("square/{input}/validation", requestHandler::squareHandlerWithValidation)
+            .onError(InputValidationException::class.java, exceptionHandler())
+            .build()
+    }
+
+    private fun exceptionHandler(): BiFunction<Throwable, ServerRequest, Mono<ServerResponse>> {
+        return BiFunction { error, request ->
+            val exception = error as InputValidationException
+            val response = InputFailedValidationResponse(
+                exception.errorCode,
+                exception.input,
+                exception.message ?: ""
+            )
+            ServerResponse.badRequest().bodyValue(response)
+        }
+    }
+}
